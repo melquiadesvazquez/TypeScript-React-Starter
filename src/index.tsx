3757068a77039fbad6439820f4848d9a6970fc9c
Melquiades Vazquez <melquiades.vazquez@gmail.com>
2018-12-06 11:42:23 +0100

    working

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+import { EnthusiasmAction } from './actions/index';
+
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+const devtools: any = window['devToolsExtension'] ? window['devToolsExtension']() : (f: any) => f;
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript'
+}, devtools);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
